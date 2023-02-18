bcc7b5abae4f142ba9b1ccb96748f85fe9fc05a1
Hryhorii Hlushko <escagt82@gmail.com>
2023-02-18 18:49:08 +0200

    add story-book, add StyleDecorator & ThemeDecorator

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -4,8 +4,8 @@ import { ButtonHTMLAttributes, FC } from 'react';
 import cls from './Button.module.scss';
 
 export enum ThemeButton {
-
   CLEAR = 'clear',
+  OUTLINE = 'outline',
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {

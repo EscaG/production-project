a78819a68f249e576340049d2263ebc582293c24
Hryhorii Hlushko <escagt82@gmail.com>
2023-03-03 01:38:04 +0200

    add redux store, add first entity

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import { BugButton } from 'app/providers/ErrorBoundary';
+import { Counter } from 'entities/Counter';
 
 const MainPage:FC = () => {
   const { t } = useTranslation('main');
@@ -8,7 +9,6 @@ const MainPage:FC = () => {
   return (
     <div>
       {t('Главная')}
-      <BugButton />
     </div>
   );
 };
